e61a46f199ca2a6ba8c2aa697f1cf3483d53e794
mansurarief <marief@andrew.cmu.edu>
2021-10-29 13:19:32 -0400

    added new post

---
+++ static/js/posts.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+  $('.messageSender').on('click', '.get_result', function(){
+     var val1 = "The current user";
+     var val2 = $("#user_post_msg").val();
+     $.ajax({
+      url: "/fetch_post",
+      type: "get",
+      data: {val1: val1, val2:val2},
+      success: function(response) {            
+        const PostItem = ({ id, message, user_fullname, avatar_img, timestamp}) => `
+        <!-- post starts -->
+        <div class="post" id="${id}">
+          <div class="post__top">
+            <img
+              class="user__avatar post__avatar"
+              src=${avatar_img}
+              alt=""
+            />
+            <div class="post__topInfo">
+              <h3>${user_fullname}</h3>
+              <p>${timestamp}</p>
+            </div>
+          </div>
+
+          <div class="post__bottom">
+            <p>${message}</p>       
+            <div id="badges-${id}"></div>
+          </div>
+
+
+          <div class="post__options">
+            <div class="post__option">
+              <span class="material-icons"> thumb_up </span>
+              <p>Like</p>
+            </div>
+
+            <div class="post__option">
+              <span class="material-icons"> chat_bubble_outline </span>
+              <p>Comment</p>
+            </div>
+
+            <div class="post__option">
+              <span class="material-icons"> near_me </span>
+              <p>Share</p>
+            </div>
+          </div>
+
+        </div>
+        <!-- post ends -->
+        `;
+
+        $('.new_posts').html([response].map(PostItem).join('')); 
+        var badge_id = 'badges-'+response.id;
+        
+        var badges_element = document.getElementById(badge_id);
+
+        for(var key in response.pred){
+          badge_content = '<span class="badge-common" id="'+key+'-'+response.id+'">'+key+' : '+response.pred[key]+'</span>';
+
+         badges_element.innerHTML = badges_element.innerHTML + badge_content;
+        }
+        
+
+
+      },
+     });
+  });
+});  
--- webserver.py
+++ webserver.py
@@ -1,7 +1,8 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request, jsonify
 import json
 from detoxify import Detoxify
-
+import datetime
+import time
 
 app = Flask(__name__)
 model = Detoxify('original', device='cpu')
@@ -21,7 +22,7 @@ def post():
 
 @app.route('/form')
 def form():
-    return render_template('form.html')    
+    return render_template('jquery_data.html')    
 
 @app.route('/data', methods = ['POST', 'GET'])
 def data():
@@ -30,6 +31,32 @@ def data():
     if request.method == 'POST':
         form_data = request.form
         return render_template('data.html',form_data = form_data)
+
  
  
+@app.route('/calculate_result')
+def calculate_result():
+  a = int(request.args.get('val1'))
+  b = int(request.args.get('val2'))
+  return jsonify({"result":a+b})
+
+@app.route('/fetch_result')
+def fetch_result():
+  a = request.args.get('val1')
+  b = request.args.get('val2')
+  return jsonify({"message":b})
+
+@app.route('/fetch_post')
+def fetch_post():
+  a = request.args.get('val1')
+  b = request.args.get('val2')
+  now = datetime.datetime.now()
+  date_text = now.strftime("%d %B %Y at %H:%M")
+  pred = model.predict(b)
+  uniq_id = str(time.time()).replace('.','')
+  pred = {str(key): str(value.round(3)) for key, value in pred.items()}
+  out = {"id": uniq_id, "message":b, "user_fullname": a, "avatar_img": 'https://i.pravatar.cc/100', "timestamp": date_text, "pred":pred}
+  return jsonify(out)
+
+
 app.run(host='localhost', port=5000, debug=True)
