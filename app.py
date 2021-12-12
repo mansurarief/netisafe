ec7b1bb6ecfb11bda054312495b2aa6f0839ac15
mansurarief <marief@andrew.cmu.edu>
2021-12-12 14:55:51 -0500

    updated app.py

--- app.py
+++ app.py
@@ -272,4 +272,4 @@ class CustomJSONizer(json.JSONEncoder):
             if isinstance(obj, np.bool_) \
             else super().default(obj) 
 
-app.run(host='0.0.0.0')
+app.run(host='0.0.0.0', port=5000, debug=True)
